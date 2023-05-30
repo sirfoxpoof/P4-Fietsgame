1f4eef5be5df0f3fab92a45f7380592dbd7e4f5d
Doetzen <Doutzenhoekstra06@gmail.com>
2023-05-30 13:13:59 +0200

    error weg

--- Assets/Scripts/Dood/Collisione.cs
+++ Assets/Scripts/Dood/Collisione.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Collisione : MonoBehaviour
 {
     public Wasd wasd;
 
     //wanneer we een object raken
     //krijgen info over de collision en daarom naam collisionInfo
-    private void OnCollisionEnter (Collision collisionInfo)
+    private void OnCollisionEnter(Collision collisionInfo)
     {
-        if(collisionInfo.GetComponent<Collider>().tag == "obstacle")
+        if (collisionInfo.collider.tag == "obstacle")
         {
             wasd.enabled = false;
         }
