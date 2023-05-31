e44e04a211e9b2cdb030cef14f0ed4476868811f
sirfoxpoof <daniquedrost4@gmail.com>
2023-05-31 13:01:11 +0200

    Nieuwe huizen + prefabs getest in de tutorial scene

---
+++ Assets/Scripts/UIScripts/Splash.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    
+    private void Start()
+    {
+        StartCoroutine(SplashScreen());
+        
+    }
+    public IEnumerator SplashScreen()
+    {
+        yield return new WaitForSeconds(12f);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
