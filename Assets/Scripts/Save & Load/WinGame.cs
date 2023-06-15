d9c72d31828f6bb9d3320df61877e32db6782174
Doetzen <Doutzenhoekstra06@gmail.com>
2023-06-15 12:23:34 +0200

    SAVE EN LOAD WERKT WHOOOOOOOO

--- Assets/Scripts/Save & Load/WinGame.cs
+++ Assets/Scripts/Save & Load/WinGame.cs
@@ -9,23 +9,19 @@ public class WinGame : MonoBehaviour
     public Wasd wasd;
     private void OnCollisionEnter(Collision other)
     {
-        if( other.collider.tag == "Player")
-        {
-            gameWon = true;
-            wasd.enabled = false;
-            print("you win!");
-        }
+        gameWon = true;
+        wasd.enabled = false;
+        print("you win!");
+      
     }
 
     private void Update()
     {
         if (gameWon)
         {
-            print("gameWOn is true");
             if(PlayerPrefs.GetInt("levelsDone") < levelNumberSave)
-            {// we willen alleen overwriten als je minder levels gedaan hebt dan je hebt. als je dan het level klaar hebt mag hij overwriten om het volgende level te spelen.
+            {
                 PlayerPrefs.SetInt("levelsDone", levelNumberSave);
-                //zetten een int naar hoeveel levels van levelNumberSave klaar zijn.
             }
         }
     }
